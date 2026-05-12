0c4251ba92f295a40fc373e51be410e85979048e
becker693 <u202419462@upc.edu.pe>
2026-05-12 18:47:16 -0500

    feat: integrate main plans view and update application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,26 @@
 import { Routes } from '@angular/router';
-import { Profile } from './iam/presentation/views/profile/profile';
+import { Home } from './shared/presentation/views/home/home';
+
+// import { Profile } from './iam/presentation/views/profile/profile';
+// import { Support } from './patients/presentation/views/support/support';
+// import { Patients } from './patients/presentation/views/patients/patients';
+// import { Notifications } from './notifications/presentation/views/notifications/notifications';
+
 import { Plans } from './suscription/presentation/views/plans/plans';
-import { Support } from './patients/presentation/views/support/support';
-import { Patients } from './patients/presentation/views/patients/patients';
-import { Notifications } from './notifications/presentation/views/notifications/notifications';
-import {Home} from './shared/presentation/views/home/home';
 
 const baseTitle = 'Vital Care';
-const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
-
+const pageNotFound = () =>
+  import('./shared/presentation/views/page-not-found/page-not-found').then((m) => m.PageNotFound);
 
 export const routes: Routes = [
-  { path: 'home',         component: Home,              title: `${baseTitle} - Home` },
+  { path: 'home', component: Home, title: `${baseTitle} - Home` },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'patients', component: Patients },
+
+  // { path: 'patients', component: Patients },
+  // { path: 'support', component: Support },
+  // { path: 'profile', component: Profile },
+  // { path: 'notification', component: Notifications },
+
   { path: 'plans', component: Plans },
-  { path: 'support', component: Support },
-  { path: 'profile', component: Profile },
-  { path: 'notification', component: Notifications },
-  { path: '**',       loadComponent: pageNotFound,  title: `${baseTitle} - Page Not Found` },
+  { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` },
 ];
