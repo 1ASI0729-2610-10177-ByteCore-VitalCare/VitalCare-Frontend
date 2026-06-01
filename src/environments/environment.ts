2c6d01f5160675759514e703f3c676a7f62c4b35
franco <francodiegorioja@gmail.com>
2026-06-01 01:43:59 -0500

    fix: correct API base URL to localhost:3001 in feature/patients

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  platformProviderApiBaseUrl: 'http://localhost:3001/',
   platformProviderVitalCareUsUsp: 'https://6a051f15aa826ca75c097e34.mockapi.io/api/v1',
   platformProviderVitalCareSubPat: 'https://6a05227baa826ca75c0981d6.mockapi.io/api/v1',
   platformProviderVitalCarePatcVs: 'https://6a0523f9aa826ca75c09838a.mockapi.io/api/v1',
